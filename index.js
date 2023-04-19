0245b02dd2fab52d26ae1452147fcb2b5b16c5dd
ledinhthany <117443401+ledinhthany@users.noreply.github.com>
2023-04-20 06:10:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const express = require('express');
 const app = express();
+const https = require('https');
+const fs = require('fs');
+const { Server } = require('socket.io');
 
+const options = {
+  key: fs.readFileSync('/path/to/your/ssl/key.pem'),
+  cert: fs.readFileSync('/path/to/your/ssl/cert.pem')
+};
 
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+const server = https.createServer(options, app);
 const io = new Server(server, {
   cors: {
     origin: ["https://ledinhthany.github.io", "https://example.com"]
   }
 });
 
-
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-app.use(express.static('public'));
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
+// handle connection event
 io.on('connection', (socket) => {
   console.log('user connected');
   socket.on('user-chat', data => {
@@ -32,10 +25,14 @@ io.on('connection', (socket) => {
   })
 });
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
