6099d78448c2d2c4796af4d7560fc780b6e0a33d
ledinhthany <117443401+ledinhthany@users.noreply.github.com>
2023-04-19 18:00:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: ["https://ledinhthany.github.io", "https://example.com"]
+  }
+});
+
+
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('user-chat', data => {
+    console.log({data})
+    io.emit('user-chat', data);
+  })
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
